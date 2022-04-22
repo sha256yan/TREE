119c73eee51d7ea5b5cda77741b96b8ba6c29435
Xiaocong Zhang <xiaocongz@student.unimelb.edu.au>
2022-04-22 10:06:22 +1000

    Add Safe Math Interface.

--- smart-contracts/contracts/GovContract.sol
+++ smart-contracts/contracts/GovContract.sol
@@ -6,6 +6,30 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 //use safe transfer libraries.
 //use safemath
 
+//Safe Math Interface
+
+contract SafeMath {
+
+    function safeAdd(uint a, uint b) public pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+
+    function safeSub(uint a, uint b) public pure returns (uint c) {
+        require(b <= a);
+        c = a - b;
+    }
+
+    function safeMul(uint a, uint b) public pure returns (uint c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+
+    function safeDiv(uint a, uint b) public pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
 
 /*
     TO DO:
@@ -72,5 +96,3 @@ contract Governance {
 
 
 }
-
-
